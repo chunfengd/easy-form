3dcd9794000345d91e70d556eaaadf840203e6cb
chunfeng <chunfeng@xiaohuihui>
2014-06-04 18:30:18 -0400

    init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to easy-form
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/easy_form/core_test.clj
@@ -0,0 +1,7 @@
+(ns easy-form.core-test
+  (:require [clojure.test :refer :all]
+            [easy-form.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
