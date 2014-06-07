321b8fd2ca47cafd5d65058baf7b5187c563e56e
chunfeng <chunfeng@xiaohuihui>
2014-06-06 23:24:39 -0400

    add functions

--- src/easy_form/core.clj
+++ src/easy_form/core.clj
@@ -1,6 +1,106 @@
-(ns easy-form.core)
+(ns easy-form.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(declare parse-element* parse-seq*)
+
+(defn- parse-element* [ancesters item]
+  (let [[tag & more] item
+        tag (name tag)
+        [tag id] (str/split tag #"\.")
+        [option & children] (cond
+                             (empty? more) [{}]
+                             (-> more first map?) more
+                             :else (cons {} more))
+        element (merge {:tag (keyword tag)
+                        :id (keyword id)}
+                       option)
+        {id :id label :label} element
+        label (or label (and id (-> id name str/capitalize)))
+        next-ancesters (if id
+                         (conj ancesters id)
+                         ancesters)
+        name (->> next-ancesters
+                  (map name)
+                  (str/join "."))]
+    (assoc element
+      :children (parse-seq* next-ancesters children)
+      :gid (keyword name)
+      :name name
+      :label label)))
+
+(defn- parse-seq* [ancesters items]
+  (->> items
+       (mapcat
+        (fn [item]
+          (cond
+           (keyword? item) (list (parse-element* ancesters [item]))
+           (vector? item) (list (parse-element* ancesters item))
+           (seq? item) (parse-seq* ancesters item))))))
+
+(defn parse-form [item]
+  (cond
+   (keyword? item) (parse-element* [] [item])
+   (vector? item) (parse-element* [] item)
+   (seq? item) (parse-seq* [] item)))
+
+(defn params->values [params item & [[values errors]]]
+  (let [{:keys [id children name ancesters]} item
+        ;; _ (println "values & erros" values errors)
+        [values errors] (reduce #(params->values params %2 %1)
+                                [values errors]
+                                children)
+        value (params name)
+        ;; _ (println (str {:id id :value value}))
+        ;; _ (println item)
+        values (if value
+                 (assoc-in values (conj ancesters id) value)
+                 values)]
+    [values errors]))
+
+(declare render-form)
+
+(def default-style
+  {:default (fn [{:keys [tag id name]} style]
+              [:input {:type tag :id id :name name}])
+   :group (fn [{:keys [tag id name children]} style]
+            [:div
+             (render-form children style)])
+   :form (fn [{:keys [tag id name children attr]} style]
+           [:form attr
+            (render-form children style)])})
+
+(defn create-horizontal-style [label-width input-width]
+  {:default (fn [{:keys [tag id name label]} style]
+              [:div.form-group
+               [:label {:class (str "control-label "
+                                    "col-sm-" label-width)
+                        :for name}
+                label]
+               [:div {:class (str "col-sm-" input-width)}
+                [:input.form-control
+                 {:type tag
+                  :id name
+                  :name name}]]])
+   :group (fn [{:keys [tag id name label children]} style]
+            [:field-set {:id name}
+             [:h1 label]
+             (render-form children style)])
+   :form (fn [{:keys [tag id name children attr]} style]
+           [:form.form-horizontal attr
+            (render-form children style)])
+   :submit (fn [{:keys [tag id name label]} style]
+             [:div.form-group
+              [:div {:class (str "col-sm-offset-" label-width " "
+                                 "col-sm-" input-width)}
+               [:button.btn.btn-primary {:type :submit}
+                label]]])
+   })
+
+(defn render-element [item style]
+  (let [{tag :tag} item]
+    ((or (tag style) (:default style)) item style)))
+
+(defn render-form [item style]
+  (cond
+   (map? item) (render-element item style)
+   (seq? item) (for [x item] (render-form x style))))
--- test/easy_form/core_test.clj
+++ test/easy_form/core_test.clj
@@ -7,4 +7,55 @@
 
 (fact
  "test demo"
- (str/split "a/b/c" #"/") => ["a" "b" "d"])
+ (str/split "a/b/c" #"/") => ["a" "b" "c"])
+
+(facts
+ "parse single element"
+ (fact "basic"
+       (parse-form [:text.username])
+       =>
+       (just {:tag :text
+              :id :username
+              :name "username"
+              :children ()
+              :gid :username
+              :label "Username"}))
+ (fact "extra option"
+       (parse-form [:text.username {:extra "added"}])
+       =>
+       (just {:tag :text
+              :id :username
+              :name "username"
+              :children ()
+              :gid :username
+              :label "Username"
+              :extra "added"}))
+ (fact "new id"
+       (parse-form [:text.username {:id :new-id}])
+       =>
+       (just {:tag :text
+              :id :new-id
+              :name "new-id"
+              :children ()
+              :gid :new-id
+              :label "New-id"}))
+ (fact "customized label"
+       (parse-form [:text.username
+                    {:name "new-name"
+                     :label "Username"}])
+       =>
+       (just {:tag :text
+              :id :username
+              :name "username"
+              :children ()
+              :gid :username
+              :label "Username"})))
+
+#_(fact "form with children"
+        (parse-form [:group.user {:level 1}
+                     :text.username
+                     (list
+                      :number.age)
+                     [:empty {:id "added"}]
+                     [:group.account
+                      :number.id]]))
