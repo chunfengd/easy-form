813374839a22d0bfb0423114dd435a06cbdc36ce
chunfeng <chunfeng@xiaohuihui>
2014-08-05 22:35:18 -0400

    version 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject easy-form "0.1.0-SNAPSHOT"
+(defproject easy-form "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.reader "0.8.5"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+  :signing {:gpg-key "chunfengd@gmail.com"})
