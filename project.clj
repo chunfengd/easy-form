2115000fcb2d587eb80544274ffb485b710ef7aa
chunfeng <chunfeng@huitu>
2015-04-30 15:34:16 -0400

    version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject easy-form "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url ""
+(defproject easy-form "0.1.1-SNAPSHOT"
+  :description "Create web forms for clojure"
+  :url "https://github.com/chunfengd/easy-form/"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.reader "0.8.5"]]
+                 [org.clojure/tools.reader "0.8.5"]
+                 [dire "0.5.2"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
   :signing {:gpg-key "chunfengd@gmail.com"})
